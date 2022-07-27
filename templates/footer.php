749b82aa00de3a6613b446792ca183361b133b77
@nguyenvankhoa <nguyenvankhoa@gapo.com.vn>
2022-07-27 18:10:18 +0700

    init project

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,3 @@
 <footer class="footer">
-  <a href="#">
-    <img src="./images/logo.png" alt="logo-footer" />
-  </a>
-  <p class="footer-description">Ứng dụng xem bóng đá trực tuyến hàng đầu Việt Nam!</p>
+  <h1>FOOTER</h1>
 </footer>
-<!-- /.footer -->
-
-<!-- JAVASCRIPT-->
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/slick.min.js"></script>
-<script src="js/main.js"></script>
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,4 @@
 <header class="main-header">
-	<div class="container">
-		<div class="row g-0 justify-content-between align-items-center">
-			<div class="col-auto col-logo">
-				<a href="#" class="logo">
-					<img src="images/logo.png" alt="Bong lau">
-				</a>
-			</div>
-		</div>
-	</div>
+	<h1>HEADER</h1>
 </header>
 <!-- /.main-header -->
